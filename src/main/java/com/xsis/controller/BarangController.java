2913369e3672644c539e7c474aa9e38b42d3a28c
ulummunga <ulum.munga@gmail.com>
2019-06-26 14:57:08 +0700

    third commit

---
+++ src/main/java/com/xsis/controller/BarangController.java
@@ -0,0 +1,38 @@
+package com.xsis.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.xsis.service.BarangService;
+import com.xsis.model.Barang;
+
+
+@Controller
+public class BarangController {
+	
+	@Autowired
+	private BarangService brngsr;
+	
+	
+	@RequestMapping ("/barang")
+	public String barang() {
+		return ("barang/barang");
+	}
+	@RequestMapping("/listbarang")
+	public String listbarang (Model mdl) {
+		List<Barang> barangs = brngsr.listall();
+		
+		mdl.addAttribute("keybarang",barangs);
+		return ("barang/listbarang");
+	}
+	@RequestMapping ("/NewBarang")
+	public String newBarang() {
+		return ("barang/NewBarang");
+	}
+
+}
--- src/main/java/com/xsis/model/Barang.java
+++ src/main/java/com/xsis/model/Barang.java
@@ -4,17 +4,80 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.id;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="barang")
 public class Barang {
-	@id
-	@Column (name="id", nullable=false)
-	@generatedValue(Strategy=GenerationType.IDENTITY)
+
+	@Id
+	@Column(name="id", nullable=false)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
-	@Column("'namaBarang", nullable = false, length = 50)
-	private String namaBarang;
+	@Column(name="namabarang",nullable=false, length = 50)
+	private String namabarang;
+	
+	@Column(name="jenisbarang", length = 30)
+	private String jenisbarang;
+	
+	@Column(name="kotapembuat", length = 30)
+	private String kotapembuat;
+	
+	@Column(name="jmlbarang")
+	private int jmlbarang;
+	
+	@Column(name="hargabarang")
+	private int hargabarang;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNamabarang() {
+		return namabarang;
+	}
+
+	public void setNamabarang(String namabarang) {
+		this.namabarang = namabarang;
+	}
+
+	public String getJenisbarang() {
+		return jenisbarang;
+	}
+
+	public void setJenisbarang(String jenisbarang) {
+		this.jenisbarang = jenisbarang;
+	}
+
+	public String getKotapembuat() {
+		return kotapembuat;
+	}
+
+	public void setKotapembuat(String kotapembuat) {
+		this.kotapembuat = kotapembuat;
+	}
+
+	public int getJmlbarang() {
+		return jmlbarang;
+	}
+
+	public void setJmlbarang(int jmlbarang) {
+		this.jmlbarang = jmlbarang;
+	}
+
+	public int getHargabarang() {
+		return hargabarang;
+	}
+
+	public void setHargabarang(int hargabarang) {
+		this.hargabarang = hargabarang;
+	}
+
+
 }
